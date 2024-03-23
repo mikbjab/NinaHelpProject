5f7316f680074aea62cf4e7eb8c8d8e4b3b69ed6
mikbjab <mb.jablonski@student.uw.edu.pl>
2024-03-23 21:44:04 +0100

    Zadanie 4 z labów, dodano posortowanie

--- src/Zad4/setOperations.java
+++ src/Zad4/setOperations.java
@@ -2,7 +2,7 @@ package Zad4;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import static java.lang.System.out;
@@ -13,8 +13,8 @@ public class setOperations {
         out.println("Enter set operations:");
         String operationsLine=s.nextLine();
         String[] elements=operationsLine.split("[\\]\\[]",0);
-        Set<Integer> firstSet= Arrays.stream(elements[1].split(",")).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toSet());
-        Set<Integer> secondSet= Arrays.stream(elements[3].split(",")).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toSet());
+        TreeSet<Integer> firstSet= Arrays.stream(elements[1].split(",")).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toCollection(TreeSet::new));
+        TreeSet<Integer> secondSet=Arrays.stream(elements[3].split(",")).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toCollection(TreeSet::new));
         String operation=elements[2].trim();
         switch (operation) {
             case "+" -> firstSet.addAll(secondSet);
